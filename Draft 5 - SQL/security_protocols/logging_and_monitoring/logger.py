26b21283830f07bb3a416508ddbb0e8f4bd3677c
CK03000 <161403504+CK03000@users.noreply.github.com>
2025-04-22 14:43:08 +1000

    Update logger.py

--- Draft 5 - SQL/security_protocols/logging_and_monitoring/logger.py
+++ Draft 5 - SQL/security_protocols/logging_and_monitoring/logger.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 logs = []
 
-def log_activity(user_id, action):
+def log_activity(subject, action, ip=None):
+    """Subject can be user_id or 'HONEYPOT' or 'SYSTEM'."""
     logs.append({
-        "user_id": user_id,
+        "user_id": subject,
         "action": action,
+        "ip": ip,
         "timestamp": datetime.now()
     })
 
